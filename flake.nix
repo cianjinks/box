21b66583f62c2c61a1d53d8a86cd44bc2fd8c3f7
Cian Jinks <cjinks99@gmail.com>
2025-12-22 12:10:10 +0000

    Initial project setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.delve
+            # gopls, goimports, etc
+            pkgs.gotools
+          ];
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cianjinks/box
+
+go 1.25.4
